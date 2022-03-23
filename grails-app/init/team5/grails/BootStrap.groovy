5c4cb3a16b06cbbcad5b97802557e96ac6042176
Sitraka Faniriandrandraina <sitraka@adrware.mg>
2022-03-23 14:25:08 +0300

    add Command entity

--- grails-app/init/team5/grails/BootStrap.groovy
+++ grails-app/init/team5/grails/BootStrap.groovy
@@ -85,6 +85,8 @@ class BootStrap {
                         }
                     }
                     utilisateurInstance.addToProduits(produitInstance)
+                    def commandeInstance = new Commande(utilisateur: utilisateurInstance,produit: produitInstance,nombre: (i+1),statut: Commande.STATUS_EN_ATTENTE,prixTotal: ((i+1)*12))
+                    commandeInstance.save()
 
             }
             utilisateurInstance.save()
