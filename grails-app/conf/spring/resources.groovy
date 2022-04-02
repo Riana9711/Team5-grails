0de88d4d2dd41763f1baefee48dc99304af0882f
Riana9711 <riana.back@gmail.com>
2022-04-02 17:01:28 +0300

    refactor : modifier locale i18n en fr

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,5 +1,10 @@
+import org.springframework.web.servlet.i18n.SessionLocaleResolver
 import team5.grails.UtilisateurPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
     utilisateurPasswordEncoderListener(UtilisateurPasswordEncoderListener)
+
+    localResolver(SessionLocaleResolver){
+        defaultLocale = new java.util.Locale('fr');
+    }
 }
